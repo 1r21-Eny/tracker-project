ba6b34529ccfa5edf7f16392a323fc257efaed5b
1r21-Eny <e.trapani.uk@gmail.com>
2025-06-07 09:11:21 +0100

    Main files and code
    To do this we need the files. Not the most efficient way to do but it should work having every tracker in each seperate file because im lazy to learn how to put in 1

---
+++ summerhmwk.py
@@ -0,0 +1,17 @@
+#menu
+def MainMenu():
+    print("Sleep Tracking: (1)")
+    print("Mood Tracking: (2)")
+    print("Spending Tracking: (3)")
+    print("Book Logs: (4)")
+    print("Game Tracking: (5)")
+    print("Study Tracking: (6)")
+    print("Exit: (7)")
+#Sleep tracking
+#Mood tracking
+#Spending tracker
+#Book logs
+#game tracker
+#study tracking
+#main loop
+MainMenu()
