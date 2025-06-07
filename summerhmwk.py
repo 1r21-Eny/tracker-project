d27c7f0de503c133c68234785ab1f1eb876a4615
1r21-Eny <e.trapani.uk@gmail.com>
2025-06-07 10:41:39 +0100

    remove a log
    removing a entry for sleep can now be done along with the rewrite to open as f so i can copy and paste when all ifs are done and only have to change with open

--- summerhmwk.py
+++ summerhmwk.py
@@ -1,6 +1,6 @@
 import time
 def MainMenu():
-    print("Sleep Tracking: (1)")
+    print("\nSleep Tracking: (1)")
     print("Mood Tracking: (2)")
     print("Spending Tracking: (3)")
     print("Book Logs: (4)")
@@ -9,7 +9,7 @@ def MainMenu():
     print("Exit: (7)")
 
 def Process():
-    print("Append: (1)")
+    print("\nAppend: (1)")
     print("Read: (2)")
     print("Remove log: (3)")
     print("rewrite: (4)")
@@ -18,29 +18,41 @@ def Process():
 
 def SleepTracking(x):
     if x == 1:
-        with open("SleepTracking.txt", "a") as st:
+        with open("SleepTracking.txt", "a") as f:
             SleepTime = int(input("How many hours did you sleep (round to nearest hour): "))
-            st.write(f"{SleepTime} hours of sleep\n")
+            f.write(f"{SleepTime} hours of sleep\n")
             time.sleep(0.5)
             print("Entry added\n")
             time.sleep(0.5)
     if x == 2:
-        with open("SleepTracking.txt", "r") as st:
-            for NewLine in st:
+        with open("SleepTracking.txt", "r") as f:
+            for NewLine in f:
                 print(NewLine.strip())
                 time.sleep(1)
             print("\n")
     if x == 3:
-        with open("SleepTracking.txt", "w") as st:
-            pass
+        with open("SleepTracking.txt", "r") as f:
+            lines = f.readlines()
+            for NewLine, line in enumerate(lines):
+                print(f"{NewLine + 1}: {line.strip()}")
+                time.sleep(1)
+            ToDelete = int(input("Enter the number for the log to delete: ")) -1
+            del lines[ToDelete]
+            print("\nupdated logs")
+            for NewLine, line in enumerate(lines):
+                print(f"{NewLine + 1}: {line.strip()}")
+        with open("SleepTracking.txt", "w") as f:
+            for line in lines:
+                f.write(line)
+        print("\n Log deleted \n")
     if x == 4:
-        with open("SleepTracking.txt", "4") as st:
+        with open("SleepTracking.txt", "4") as f:
             pass
     if x == 5:
-        with open("SleepTracking.txt", "r") as st:
+        with open("SleepTracking.txt", "r") as f:
             pass
     if x == 6:
-        with open("SleepTracking.txt", "r") as st:
+        with open("SleepTracking.txt", "r") as f:
             pass
 def MoodTracking(x):
     pass
@@ -66,21 +78,21 @@ while True: #mainloop
             if TrackerChoice <= 7 and TrackerChoice >= 1:
                 break
             else:
-                print("Enter number between 1 and 7")
+                print("\nEnter number between 1 and 7")
                 continue
         except ValueError:
-            print("Enter valid integer")
+            print("\nEnter valid integer")
     if TrackerChoice == 7:
         while True:
             try:
-                leaving = int(input("Do you want to exit?\n yes(1)/no(2): "))
+                leaving = int(input("\nDo you want to exit?\n yes(1)/no(2): "))
                 if leaving == 1 or leaving == 2:
                     break
                 else:
-                    print("Enter values 1(yes) or 2(no)")
+                    print("\nEnter values 1(yes) or 2(no)")
                     continue
             except ValueError:
-                print("enter valid integer")
+                print("\nenter valid integer")
         if leaving == 1:
             print("bye")
             break
@@ -95,9 +107,9 @@ while True: #mainloop
             if ProcessChoice >= 1 and ProcessChoice <= 6:
                 break
             else:
-                print("Enter valid number between 1 and 6")
+                print("\nEnter valid number between 1 and 6")
         except ValueError:
-            print("Enter an Integer")
+            print("\nEnter an Integer")
     if TrackerChoice == 1:
         SleepTracking(ProcessChoice)
     if TrackerChoice == 2:
